e0bcc38e0a62da2bfc06c27d2d855b7f160c348d
Tom <tomw08@gmail.com>
2015-11-08 16:45:50 +0000

    update tests

--- Test/Test_encode.py
+++ Test/Test_encode.py
@@ -4,34 +4,29 @@ import sys
 # Add the ptdraft folder path to the sys.path list
 sys.path.append('/Users/Tom/PycharmProjects/vector_class/Vector_Calc_py')
 
-from index import a, b
-from index import Vector
+from CaesarCipher_py import index
+
+obj = index.CaesarCipher(1)
+
+obj2 = index.CaesarCipher(3)
+
+class encoding(unittest.TestCase):
+    def test_1_shift_encode(self):
+        a = obj.encode('z')
+        self.assertEqual(chr(a[0]), 'a')
 
 
-class __init__(unittest.TestCase):
-    def test_length_property(self):
-        self.assertEqual(a.length, 3)
+class decoding(unittest.TestCase):
+    def test_1_shift_decode(self):
+        a = obj.encode('z')
+        self.assertEqual(chr(a[0]), 'a')
 
-class __str__(unittest.TestCase):
+
+class toOrd(unittest.TestCase):
     def test_(self):
         self.assertEqual(str(a), '(1,2,3)')
 
-class Add(unittest.TestCase):
-    def test_returns_vector_object(self):
-        vec1 = a.add(a)
-        self.assertEqual(vec1.__class__.__name__, 'Vector')
 
-    def test_adds_properly(self):
-        vec2 = a.add(a)
-        self.assertEqual(vec2.array, [4, 6, 8])
-
-class Subtract(unittest.TestCase):
-    pass
-
-
-class Norm(unittest.TestCase):
-    pass
-
-
-class Dot(unittest.TestCase):
-    pass
+class toChr(unittest.TestCase):
+    def test_(self):
+        self.assertEqual(str(a), '(1,2,3)')
