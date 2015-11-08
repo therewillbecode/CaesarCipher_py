5ef87081735df50f1545fd8b2eaf277e70cbb1ad
Tom <tomw08@gmail.com>
2015-11-08 21:05:56 +0000

    f

--- Test/Test_encode.py
+++ Test/Test_encode.py
@@ -8,19 +8,25 @@ from CaesarCipher_py import index
 
 obj = index.CaesarCipher(1)
 
-obj2 = index.CaesarCipher(3)
+obj2 = index.CaesarCipher(2)
 
-class encoding(unittest.TestCase):
+class shift_1(unittest.TestCase):
     def test_1_shift_encode(self):
         a = obj.encode('z')
         self.assertEqual(chr(a[0]), 'a')
 
-
-class decoding(unittest.TestCase):
     def test_1_shift_decode(self):
-        a = obj.encode('z')
+        a = obj2.encode('z')
         self.assertEqual(chr(a[0]), 'a')
 
+class shift_2(unittest.TestCase):
+    def test_2_shift_encode(self):
+        a = obj.encode('z')
+        self.assertEqual(chr(a[0]), 'b')
+
+    def test_2_shift_decode(self):
+        a = obj2.decode('a')
+        self.assertEqual(chr(a[0]), 'y')
 
 class toOrd(unittest.TestCase):
     def test_(self):
