f66bc07f0f8293620bf4653db03fa63a652046d8
Tom <tomw08@gmail.com>
2015-11-08 16:51:23 +0000

    update index

--- index.py
+++ index.py
@@ -3,13 +3,33 @@ import math
 
 class CaesarCipher(object):
     def __init__(self, shift):
-        pass
+        self.shift = shift
 
     def encode(self, str):
-        pass
+        shifted = [self.map_encode(x, self.shift) for x in str]
+        return shifted
 
     def decode(self, str):
-        pass
+        unshifted = [chr(ord(x) - self.shift) for x in str]
+        unshifted = [self.map_decode(x, self.shift) for x in unshifted]
+        return unshifted
 
-a = CaesarCipher([1, 2, 3])
+    def map_encode(self, char, shift):
+        shifted = ord(char) + self.shift
+        if int(shifted) > 122:
+            shifted = (shifted-1) - 122 + 97
+        return shifted
 
+    def map_decode(self, char, shift):
+        shifted = ord(char) - self.shift
+        if shifted < 97:
+            shifted = 122 - ((97 - shifted) + 1)
+        return shifted
+
+a = CaesarCipher(1)
+
+
+c = a.decode('a')
+
+
+print(chr(c[0]))
