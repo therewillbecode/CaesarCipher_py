268e88c2fc10a4347eaaf8780dd3441510be3f01
Tom <tomw08@gmail.com>
2015-11-09 00:56:27 +0000

    d

--- Test/Test_encode.py
+++ Test/Test_encode.py
@@ -4,7 +4,7 @@ import sys
 # Add the ptdraft folder path to the sys.path list
 sys.path.append('/Users/Tom/PycharmProjects/vector_class/Vector_Calc_py')
 
-from CaesarCipher_py import index
+import index
 
 obj = index.CaesarCipher(1)
 
@@ -13,26 +13,27 @@ obj2 = index.CaesarCipher(2)
 class shift_1(unittest.TestCase):
     def test_1_shift_encode(self):
         a = obj.encode('z')
-        self.assertEqual(chr(a[0]), 'a')
+        self.assertEqual(a, 'A')
 
-    def test_1_shift_decode(self):
-        a = obj2.encode('z')
-        self.assertEqual(chr(a[0]), 'a')
+    def test_shift_decode(self):
+        a = obj2.decode('a')
+        self.assertEqual(a, 'Y')
 
 class shift_2(unittest.TestCase):
     def test_2_shift_encode(self):
-        a = obj.encode('z')
-        self.assertEqual(chr(a[0]), 'b')
+        a = obj2.encode('z')
+        self.assertEqual(a, 'B')
 
     def test_2_shift_decode(self):
         a = obj2.decode('a')
-        self.assertEqual(chr(a[0]), 'y')
+        self.assertEqual(a, 'Y')
 
-class toOrd(unittest.TestCase):
-    def test_(self):
-        self.assertEqual(str(a), '(1,2,3)')
+class shift_1_for_abc(unittest.TestCase):
+    def test_2_shift_encode(self):
+        a = obj.encode('abc')
+        self.assertEqual(a, 'BCD')
 
+    def test_2_shift_decode(self):
+        a = obj.decode('abc')
+        self.assertEqual(a, 'ZAB')
 
-class toChr(unittest.TestCase):
-    def test_(self):
-        self.assertEqual(str(a), '(1,2,3)')
